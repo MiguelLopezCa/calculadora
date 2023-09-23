a63703fd3e5449562c88b3ed3a6b2867ca9b85f9
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2023-09-22 19:57:29 -0500

    ya que

--- calculadora/src/app/page.js
+++ calculadora/src/app/page.js
@@ -5,51 +5,133 @@ import styles from './page.css';
 const CalculatorComponent = () => {
   const [input, setInput] = useState('');
   const [result, setResult] = useState('');
-
-  const hacerClick = (value) => {
-    if (value === '=') {
-      try {
-        setResult(eval(input).toString());
-      } catch (error) {
-        setResult('Error');
-      }
-    } else if (value === 'C') {
-      setInput('');
-      setResult('');
-    } else {
-      setInput((prevInput) => prevInput + value);
+  const agregarCero = () => {
+    setInput((prevInput) => prevInput + '0');
+  };
+  
+  const agregarUno = () => {
+    setInput((prevInput) => prevInput + '1');
+  };
+  
+  const agregarDos = () => {
+    setInput((prevInput) => prevInput + '2');
+  };
+  
+  const agregarTres = () => {
+    setInput((prevInput) => prevInput + '3');
+  };
+  
+  const agregarCuatro = () => {
+    setInput((prevInput) => prevInput + '4');
+  };
+  
+  const agregarCinco = () => {
+    setInput((prevInput) => prevInput + '5');
+  };
+  
+  const agregarSeis = () => {
+    setInput((prevInput) => prevInput + '6');
+  };
+  
+  const agregarSiete = () => {
+    setInput((prevInput) => prevInput + '7');
+  };
+  
+  const agregarOcho = () => {
+    setInput((prevInput) => prevInput + '8');
+  };
+  
+  const agregarNueve = () => {
+    setInput((prevInput) => prevInput + '9');
+  };
+  const sumar = () => {
+    if (input !== '') {
+      setInput((prevInput) => prevInput + '+');
     }
   };
+  
+  const restar = () => {
+    if (input !== '') {
+      setInput((prevInput) => prevInput + '-');
+    }
+  };
+  
+  const multiplicar = () => {
+    if (input !== '') {
+      setInput((prevInput) => prevInput + '*');
+    }
+  };
+  
+  const dividir = () => {
+    if (input !== '') {
+      setInput((prevInput) => prevInput + '/');
+    }
+  };
+  
+  const calcularResultado = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+  
+  const borrar = () => {
+    setInput('');
+    setResult('');
+  };
+  const calcularPorcentaje = () => {
+    try {
+      const resultado = eval(input);
+      const porcentaje = resultado / 100;
+      setInput(porcentaje.toString());
+      setResult('');
+    } catch (error) {
+      setInput('Error');
+      setResult('');
+    }
+  };
+  
 
   return (
     <div className={styles.container}>
       <div className={styles.label}>Calculadora</div>
 
-      <div className={styles.display}>
+      <div className={styles.resultado}>
         <input type="text" value={input} readOnly />
         <div className={styles.result}>{result}</div>
       </div>
-
-      <div className={styles.buttonsContainer}>
-        <button className={styles.numeric} onClick={() => hacerClick('7')}>7</button>
-        <button className={styles.numeric} onClick={() => hacerClick('8')}>8</button>
-        <button className={styles.numeric} onClick={() => hacerClick('9')}>9</button>
-        <button className={styles.numeric} onClick={() => hacerClick('4')}>4</button>
-        <button className={styles.numeric} onClick={() => hacerClick('5')}>5</button>
-        <button className={styles.numeric} onClick={() => hacerClick('6')}>6</button>  
-        <button className={styles.numeric} onClick={() => hacerClick('1')}>1</button>
-        <button className={styles.numeric} onClick={() => hacerClick('2')}>2</button>
-        <button className={styles.numeric} onClick={() => hacerClick('3')}>3</button>
+      
+      <div className={styles.buttons7a9}>
+        <button onClick={agregarSiete}>7</button>
+        <button onClick={agregarOcho}>8</button>
+        <button onClick={agregarNueve}>9</button>
       </div>
 
-      <div className={styles.operationsContainer}>
-        <button className={styles.operation} onClick={() => hacerClick('C')}>C</button>
-        <button className={styles.operation} onClick={() => hacerClick('%')}>%</button>
-        <button className={styles.operation} onClick={() => hacerClick('+')}>+</button>
-        <button className={styles.operation} onClick={() => hacerClick('-')}>-</button>
-        <button className={styles.operation} onClick={() => hacerClick('/')}>/</button>
-        <button className={styles.operation} onClick={() => hacerClick('*')}>*</button>
-        <button className={styles.operation} onClick={() => hacerClick('=')}>=</button>
+        <div className={styles.buttons4a6}>  
+
+        <button onClick={agregarCuatro}>4</button>
+        <button onClick={agregarCinco}>5</button>
+        <button onClick={agregarSeis}>6</button>
+        </div>
+
+        <div className={styles.buttons1a3}>
+        <button onClick={agregarUno}>1</button>
+        <button onClick={agregarDos}>2</button>
+        <button onClick={agregarTres}>3</button>
+        </div>
+        <div className={styles.button0}>
+        <button onClick={agregarCero}>0</button>
+        </div>
+
+      <div className={styles.operaciones}>
+        <button onClick={calcularPorcentaje}>%</button>
+        <button onClick={sumar}>+</button>
+        <button onClick={restar}>-</button>
+        <button onClick={multiplicar}>*</button>
+        <button onClick={dividir}>/</button>
+        <button onClick={calcularResultado}>=</button>
+        <button onClick={borrar}>C</button>
       </div>
     </div>
   );
